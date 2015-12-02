e675abac38f1617f76ee5d9eadf21d229a4fc72e
Jay Mo <jayhxmo@gmail.com>
2015-12-01 23:24:28 -0500

    Answer validation!

--- js/validate.js
+++ js/validate.js
@@ -1,11 +1,20 @@
 // validate.js
-
+var points = 0;
 function validate(input) {
-	if (input.text() == "answer") {
+	if ($(input).val() == "answer") {
 		console.log("answer");
+		$('.button--accept[data-stack = stack_buana]').click();
+		points += 100;
 	}
 
 	else {
 		console.log("wrong!");
+		$('.button--reject[data-stack = stack_buana]').click();
+		points -= 10;
+	}
+
+	$('.score').text(points + " PTS");
+	if (points < 0) {
+		$('.score').addClass("negativeRed");
 	}
 }
