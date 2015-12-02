18a093ed198d3270c7ab180eb1b16055779fe6a4
Jay Mo <jayhxmo@gmail.com>
2015-12-01 22:18:50 -0500

    Created naming section

--- js/background.js
+++ js/background.js
@@ -1,9 +1,9 @@
 fade(1);
 function fade(index) {
-	$(".introduction").removeClass("color-" + (index - 1) % 10);
-	$(".introduction").addClass("color-" + index  % 10);
+	$(".background").removeClass("color-" + (index - 1) % 10);
+	$(".background").addClass("color-" + index  % 10);
 
 	window.setTimeout(function () {
     	fade(index + 1);
-	}, 1500);
+	}, 2000);
 }
--- js/transition.js
+++ js/transition.js
@@ -8,3 +8,10 @@ function moveFromTo(from, to) {
 	$(from).addClass('visible-none');
 	$(to).removeClass('display-none');
 }
+
+function submitName(name) {
+	$('h3.username').text($(name).val());
+	console.log($(name).val());
+	console.log($('h3.username').text());
+	moveFromTo(".name", ".play");
+}
